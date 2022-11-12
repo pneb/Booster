fb39f6aa11d6ae6a407b5265ed8bd056ed2ed319
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-11-12 14:19:27 +0800

    Create utils.py

---
+++ utils/utils.py
@@ -0,0 +1,14 @@
+import multiprocessing
+
+class Utils:
+  def __init__(self):
+    self.data = None
+    self.process = None
+  
+  def execute(self, command, data=None):
+    self.data = data
+    self.process = multiprocessing.Process(target=self.command)
+    self.process.start()
+  
+  def command(self):
+    command(self.data)
