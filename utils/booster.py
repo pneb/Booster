c61c74d45b1cca078a19a8529943e2893f303a4d
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-11-12 14:19:07 +0800

    Create booster.py

---
+++ utils/booster.py
@@ -0,0 +1,13 @@
+from utils.utils import Utils
+from utils.windows import Windows
+
+class Booster:
+  def __init__(self):
+    self.utils = Utils()
+    self.windows = Windows()
+  
+  def boost(self):
+    utils.execute(self.command)
+  
+  def command(self):
+    self.windows.optimize()
