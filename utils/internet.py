d9b5e73c0784909ead8a900262ae42b6e2f44905
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-11-12 14:18:42 +0800

    Create internet.py

---
+++ utils/internet.py
@@ -0,0 +1,14 @@
+from utils.utils import Utils
+from utils.sockets import Sockets
+
+class Internet:
+  def __init__(self):
+    self.utils = Utils()
+    self.sockets = Sockets()
+  
+  def boost(self):
+    utils.execute(self.command)
+  
+  def command(self):
+    self.sockets.open()
+    self.sockets.close()
